27736c50ed4b49a0279e5799e71cacb03aee2ff5
Hantong Liu <hantongliu@outlook.com>
2022-12-04 21:37:30 -0800

    add tuit schema

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -3,5 +3,14 @@ const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
     liked: Boolean,
+    disliked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    handle: String,
+    tweets: "0k",
+    image: String,
+    time: String,
+    userName: String
 }, {collection: 'tuits'});
 export default schema;
